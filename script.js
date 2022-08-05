1b07bf0aa1ae2c335c41642101ec26ca465fd938
Kelvin Revertis <86212250+kelvinrevertis@users.noreply.github.com>
2022-08-05 13:52:59 -0300

    Start create hero

---
+++ script.js
@@ -0,0 +1,13 @@
+const btnStatus = document.getElementById("button-status")
+const inputHero = document.getElementById("hero-name")
+const initialStatus = [{str: 1,def: 1,hp: 5}]
+
+const showStatus =()=>{
+
+    console.log(initialStatus)
+    
+}
+
+const createHero =()=>{
+
+}
