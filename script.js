1bb5c15972774748793933ccce507340c5a8ce79
Kelvin Revertis <kelvinrev2@hotmail.com>
2022-08-07 19:51:15 -0300

    Creat battle log

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const btnStatus = document.getElementById("button-status")
 const inputHero = document.getElementById("hero-name")
 const initialStatus = [{str: 1,def: 1,hp: 5}]
+const combatLog = document.getElementById("combat-log")
 
 const showStatus =()=>{
-
-    console.log(initialStatus)
-    console.log("Teste")
+    const combat = document.createElement('li')
+    combat.innerText = `${inputHero} com status ${initialStatus}`
+    combatLog = combat
     
 }
 
