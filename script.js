77eff1c7b0f02f5d479158ec65ce46d99053515f
Kelvin Revertis <kelvinrev2@hotmail.com>
2022-08-08 22:54:49 -0300

    Create hero and printing

--- script.js
+++ script.js
@@ -2,13 +2,22 @@ const btnStatus = document.getElementById("button-status")
 const inputHero = document.getElementById("hero-name")
 const initialStatus = [{str: 1,def: 1,hp: 5}]
 const combatLog = document.getElementById("combat-log")
+const form = document.getElementById("hero-form")
 
 const showStatus =()=>{
     const combat = document.createElement('li')
-    combat.innerText = `${inputHero} com status ${initialStatus}`
-    combatLog = combat
+    combat.innerHTML = `${inputHero.value} com status ${initialStatus}`
+    console.log(combat)
     
 }
+
+form.addEventListener('submit', event=>{
+    event.preventDefault()
+
+    showStatus()
+
+})
+
 
 const createHero =()=>{
 
