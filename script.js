06d4e6e27a254e256538393ac1a967a983bdc145
Kelvin Revertis <kelvinrev2@hotmail.com>
2022-08-09 22:22:08 -0300

    Impressão de status e inicio de levelUp

--- script.js
+++ script.js
@@ -3,18 +3,34 @@ const inputHero = document.getElementById("hero-name")
 const initialStatus = [{str: 1,def: 1,hp: 5}]
 const combatLog = document.getElementById("combat-log")
 const form = document.getElementById("hero-form")
+const levelUp = document.getElementById("level-up")
 
-const showStatus =()=>{
+var showStatus = initialStatus.map(status =>{
+    return "força:" + status.str+" defesa:"+ status.def+" Vida:"+ status.hp
+
+})
+
+const showHero =()=>{
     const combat = document.createElement('li')
-    combat.innerHTML = `${inputHero.value} com status ${initialStatus}`
+    combat.innerHTML = `${inputHero.value} com status ${showStatus}`
     console.log(combat)
     
 }
 
+const upLevel=()=>{
+    let level = showStatus.map(status =>{
+        return status.str+10
+    })
+}
+
+var level = initialStatus.map(status=>{
+    return status.str+1, status.def+1, status.hp+1
+})
+
 form.addEventListener('submit', event=>{
     event.preventDefault()
 
-    showStatus()
+    showHero()
 
 })
 
