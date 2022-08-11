06f8314ba3942c18227db77af0271c93e7332587
Kelvin Revertis <kelvinrev2@hotmail.com>
2022-08-10 22:45:21 -0300

    Tentativas de Level Up

--- script.js
+++ script.js
@@ -5,27 +5,31 @@ const combatLog = document.getElementById("combat-log")
 const form = document.getElementById("hero-form")
 const levelUp = document.getElementById("level-up")
 
+/*var upStatus = initialStatus.map(status =>{
+    return  status*2
+})*/
+
+const upLevel =(stats)=>{
+    console.log(stats)
+    stats.str = 2
+    stats.def = 2
+    stats.hp = 2
+}
+
+//console.log(upStatus)
+
 var showStatus = initialStatus.map(status =>{
     return "força:" + status.str+" defesa:"+ status.def+" Vida:"+ status.hp
-
 })
 
 const showHero =()=>{
     const combat = document.createElement('li')
     combat.innerHTML = `${inputHero.value} com status ${showStatus}`
     console.log(combat)
-    
+    upLevel(showStatus)
 }
 
-const upLevel=()=>{
-    let level = showStatus.map(status =>{
-        return status.str+10
-    })
-}
 
-var level = initialStatus.map(status=>{
-    return status.str+1, status.def+1, status.hp+1
-})
 
 form.addEventListener('submit', event=>{
     event.preventDefault()
