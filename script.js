77c4dca76b8d577ce3cd3d31e88a50dea2fbd7ee
Kelvin Revertis <kelvinrev2@hotmail.com>
2022-08-15 21:30:19 -0300

    Level UP system ok!

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 const btnStatus = document.getElementById("button-status")
 const inputHero = document.getElementById("hero-name")
-const atackStats = 1
-const defStats= 1
-const hpStats = 5
+var atackStats = 1
+var defStats= 1
+var hpStats = 5
 const combatLog = document.getElementById("combat-log")
 const form = document.getElementById("hero-form")
-const levelUp = document.getElementById("level-up")
 
 
-var showStatus =  "força:" +atackStats+" defesa:"+ defStats+" Vida:"+hpStats
+function levelUP(){
+    atackStats = atackStats+1
+    defStats = defStats+1
+    hpStats = hpStats+2
+}
 
 const showHero = () => {
+    var showStatus =  "força:" +atackStats+" defesa:"+ defStats+" Vida:"+hpStats
+    levelUP()
     const combat = document.createElement('li')
     combat.innerHTML = `${inputHero.value} com status ${showStatus}`
     console.log(combat)
+
 }
 
 
